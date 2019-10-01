d9c79d50512e536ae5320ff517ffd191e88abd35
kevin6767 <48367743+kevin6767@users.noreply.github.com>
2019-10-01 15:21:25 -0500

    finished with base project

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ list.addEventListener('click', function(ev) {
 
 function newElement() {
     var li = document.createElement("li");
+    var times = document.getElementsByClassName("stopwatch")
     var inputValue = document.getElementById("toDo").value;
     var t = document.createTextNode(inputValue);
     li.appendChild(t);
@@ -38,6 +39,7 @@ function newElement() {
       alert("You must write something!");
     } else {
       document.getElementById("myUL").appendChild(li);
+      
     }
     document.getElementById("toDo").value = "";
   
@@ -54,3 +56,92 @@ function newElement() {
       }
     }
   }
+
+  var Stopwatch = function(elem, options) {
+
+    var timer       = createTimer(),
+        startButton = createButton("start", start),
+        stopButton  = createButton("stop", stop),
+        resetButton = createButton("reset", reset),
+        offset,
+        clock,
+        interval;
+  
+    // default options
+    options = options || {};
+    options.delay = options.delay || 1;
+  
+    // append elements     
+    elem.appendChild(timer);
+    elem.appendChild(startButton);
+    elem.appendChild(stopButton);
+    elem.appendChild(resetButton);
+  
+    // initialize
+    reset();
+  
+    // private functions
+    function createTimer() {
+      var watch = document.createElement("span");
+      watch.className = "timer";
+      return watch;
+    }
+  
+    function createButton(action, handler) {
+      var a = document.createElement("a");
+      a.href = "#" + action;
+      a.innerHTML = action;
+      a.addEventListener("click", function(event) {
+        handler();
+        event.preventDefault();
+      });
+      return a;
+    }
+  
+    function start() {
+      if (!interval) {
+        offset   = Date.now();
+        interval = setInterval(update, options.delay);
+      }
+    }
+  
+    function stop() {
+      if (interval) {
+        clearInterval(interval);
+        interval = null;
+      }
+    }
+  
+    function reset() {
+      clock = 0;
+      render();
+    }
+  
+    function update() {
+      clock += delta();
+      render();
+    }
+  
+    function render() {
+      timer.innerHTML = clock/1000; 
+    }
+  
+    function delta() {
+      var now = Date.now(),
+          d   = now - offset;
+  
+      offset = now;
+      return d;
+    }
+  
+    // public API
+    this.start  = start;
+    this.stop   = stop;
+    this.reset  = reset;
+  };
+
+  var elems = document.getElementsByClassName("stopwatch");
+
+for (var i=0, len=elems.length; i<len; i++) {
+  new Stopwatch(elems[i]);
+}
